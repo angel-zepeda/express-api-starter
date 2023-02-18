9aab83e7c98451b7c47047217548a9ef318c2603
angel-zepeda <angel.zepeda0508@gmail.com>
2023-02-17 19:22:37 -0600

    feat: add morgan logger with winston

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { json, Express } from 'express'
 import cors from 'cors'
 import router from './routes'
+import { logs } from './utils'
 
 const app: Express = express()
 
 app.use(json())
 app.use(cors({ origin: ['http://localhost:8080', 'http://localhost:3000'] }))
+app.use(logs().tiny)
+
 app.use('/api/v1/', router)
 
 export default app
---
+++ src/utils/logger.ts
@@ -0,0 +1,30 @@
+import { createLogger, transports, format } from 'winston'
+import morgan from 'morgan'
+
+export const logger = createLogger({
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(info => `${info.timestamp} ${info.message}`),
+  ),
+
+  transports: [
+    new transports.File({
+      filename: './logs/app.log',
+      maxsize: 5242880, // 5mb
+      maxFiles: 5,
+    }),
+    new transports.Console(),
+  ],
+})
+
+const myStream = {
+  write: (text: string) => {
+    logger.info(text)
+  },
+}
+
+export const logs = () => {
+  return {
+    tiny: morgan(':>> :method :url :status :req[header] - :response-time ms', { stream: myStream }),
+  }
+}
